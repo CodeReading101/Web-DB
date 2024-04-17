a2f35e8885ff4a65ea511984700b741d8e730e69
CodeReading101 <159878159+CodeReading101@users.noreply.github.com>
2024-04-17 12:46:29 +0900

    Update selectSQL.php

--- game/php/selectSQL.php
+++ game/php/selectSQL.php
@@ -1,30 +1,29 @@
 <?php header('Content-Type: text/html; charset=UTF-8'); ?>
 <HTML>
 	<BODY>
-		<input type="button" value="Tetris" onClick="location.href='Tetris.html'">.		
-		<input type="button" value="Snake" onClick="location.href='Snake.html'">.		
+		<input type="button" value="Tetris" onClick="location.href='Tetris.html'">		
+		<input type="button" value="Snake" onClick="location.href='Snake.html'">		
 		<BR>
-		<BR>
+		<BR> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+		<B><?php echo $_POST['title']; ?></B><BR><BR>  
 		<TABLE border=1 width=600 cellspacing=2 cellpadding=7 style="text-align:center;" >
 			<TR>
-				<TD>TITLE</TD>
+				<TD>RANK</TD>
 				<TD>SCORE</TD>
 				<TD>NAME</TD>
 			</TR>
-<?php 
-	// 이전 페이지에서 전달 받은 메시지 확인
-	$title=  $_POST['title'];
 
+<?php 
 	// MySQL 드라이버 연결 
 	include("./SQLconstants.php");
 	$conn = mysqli_connect($mySQL_host,$mySQL_id,$mySQL_password,$mySQL_database) or die ("Can't access DB");
 
 	// MySQL 책 검색 실행 및 결과 출력
-	$query = "select * from game where title = '".$title."' order by score desc;";
+	$query = "select * from game where title = '".$_POST['title']."' order by score desc;";
 	$resultSet = mysqli_query( $conn, $query );
-	while( $result = mysqli_fetch_array( $resultSet ) )
+	for( $rank=1; $result = mysqli_fetch_array( $resultSet ); $rank++ )
 	{
-		echo "\n<TR><TD>".$result['title'];
+		echo "\n<TR><TD>".$rank;
 		echo "\n</TD><TD>".$result['score'];
 		echo "\n</TD><TD>".$result['name'];
 		echo "\n</TD></TR>";
